48d003c52bedb6dc5b512939cbc06d71c6afb3a2
Shawn Shen <shenhaipeng7@gmail.com>
2026-03-09 23:23:51 -0500

    Convert inputs to FormControl and update disable logic

--- inClass/src/app/profile/profile.component.ts
+++ inClass/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,38 +8,53 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  name = "";
-  email = "";
-  DoB = "";
-  restName(){
-    this.name = "";
-  }
-  restEmail(){
-    this.email = "";
-  }
-  restDob(){
-    this.DoB = "";
-  }
+  // name = "";
+  // email = "";
+  // DoB = "";
+  name = new FormControl('');
+  email = new FormControl('');
+  DoB = new FormControl('');
+  // restName(){
+  //   this.name = "";
+  // }
+  // restEmail(){
+  //   this.email = "";
+  // }
+  // restDob(){
+  //   this.DoB = "";
+  // }
   restSave(){
+    this.name.setValue('');
+    this.email.setValue('');
+    this.DoB.setValue('');
     this.saveCount = 0;
   }
-  saveName() {
-    console.log("Saved name:", this.name);
-  }
+  // saveName() {
+  //   console.log("Saved name:", this.name);
+  // }
 
-  saveEmail() {
-    console.log("Saved email:", this.email);
-  }
+  // saveEmail() {
+  //   console.log("Saved email:", this.email);
+  // }
 
-  saveDob() {
-    console.log("Saved DOB:", this.DoB);
-  }
+  // saveDob() {
+  //   console.log("Saved DOB:", this.DoB);
+  // }
   saveCount = 0;
+  // saveProfile() {
+  //   console.log("Profile Saved:");
+  //   console.log("Name:", this.name);
+  //   console.log("Email:", this.email);
+  //   console.log("DateOfBirth:", this.DoB);
+
+  //   this.saveCount++;
+  // }
   saveProfile() {
-    console.log("Profile Saved:");
-    console.log("Name:", this.name);
-    console.log("Email:", this.email);
-    console.log("DateOfBirth:", this.DoB);
+    console.log('Profile Saved:', {
+      name: this.name.value,
+      email: this.email.value,
+      DoB: this.DoB.value
+    });
 
     this.saveCount++;
   }
